d20e653f39f7a794040c5222939d6d78fd3572f5
zahoor248 <zahoor.alphasquad@gmail.com>
2023-11-12 01:19:30 +0500

    reservation page fixes

--- src/Components/Reservations/ReservationDetails.jsx
+++ src/Components/Reservations/ReservationDetails.jsx
@@ -1,12 +1,13 @@
 import React, { useEffect, useState } from "react";
-import user1 from "../../assets/images/chat1.jpg";
+import emptyState from "../../assets/images/reservation.png";
+
 import dayjs from "dayjs";
 import { useDispatch, useSelector } from "react-redux";
 import { handleAPIRequest } from "../../helper/ApiHandler";
 import { setAllReasevation } from "../../Store/Actions/Actions";
 import { Link, useLocation } from "react-router-dom";
 const ReservationDetails = () => {
-  const [loading, setLoading] = useState(false);
+  const [loading, setLoading] = useState(true);
   const location = useLocation();
   const [reservationDetails, setReservationDetails] = useState([]);
   const [opentoAcceptoffer, setOpenToAcceptOffer] = useState([]);
@@ -27,7 +28,6 @@ const ReservationDetails = () => {
     )
       .then((response) => {
         if (response.data) {
-          console.log(response.data);
           setReservationDetails(response.data.reservation);
           let parentReservation = [response.data.reservation];
 
@@ -71,180 +71,194 @@ const ReservationDetails = () => {
           <div className="text-3xl">Offer Details</div>
         </div>
 
-        <div className="grid grid-cols-1 justify-between w-full py-10 gap-9 flex-wrap">
-          <div className="border-neutral-900 border h-fit w-full flex border-l  hover:shadow-sm  rounded-xl">
-            {" "}
-            <div className="flex justify-start w-full flex-col ">
-              <div className="flex flex-col items-start gap-4 p-8">
-                <div className="">
-                  <div className="font-bold text-[#2676BC] relative w-fit text-xl flex items-center gap-1">
-                    {" "}
-                    Offer:
-                    <div className="  absolute -right-28 px-6 bg-blue-50 !text-blue-600 border-blue-600 capitalize border rounded-full py-[2px] text-sm">
-                      {reservationDetails.status}
-                    </div>
-                  </div>
-                  <div className="flex flex-row gap-3 pt-4">
-                    <div className="font-semibold">Location:</div>
-                    <p className="">{reservationDetails.location}</p>
-                  </div>
-                  <div className="flex flex-row gap-3 pt-4">
-                    <div className="font-semibold">Date:</div>
-                    <div className="flex">
-                      <p className="">
-                        {dayjs(reservationDetails.start_date).format(
-                          "DD/MM/YYYY"
-                        )}
-                      </p>
-                      <span className="px-2">-</span>
-                      <p className="">
-                        {dayjs(reservationDetails.end_date).format(
-                          "DD/MM/YYYY"
-                        )}
-                      </p>
-                    </div>
-                  </div>
-                </div>
-                <div className="flex gap-3 w-full justify-start">
-                  <Link to={`/chats?${reservationDetails?.uuid}`}>
-                    <button className="px-6 py-3 bg-blue-600 text-white rounded-lg mt-2">
-                      Chat
-                    </button>
-                  </Link>
-
-                  {opentoAcceptoffer?.offered_by_me === false &&
-                    opentoAcceptoffer?.status === "open" && (
-                      <>
-                        <div
-                          onClick={() =>
-                            setTerms({ ...terms, toggle: !terms.toggle })
-                          }
-                        >
-                          <button className="px-6 py-3 bg-blue-600 text-white rounded-lg mt-2">
-                            Accept
-                          </button>
-                        </div>
-
-                        <Link to={`/counter-offer?${reservationDetails?.uuid}`}>
-                          <button className="px-6 py-3 bg-blue-600 text-white rounded-lg mt-2">
-                            Counter
-                          </button>
-                        </Link>
-                      </>
-                    )}
-                </div>
-                <div className="flex flex-row gap-3 pt-4">
-                  <div className="font-semibold">Description:</div>
-                  <p className="">{reservationDetails?.description}</p>
-                </div>
-              </div>
-
-              {/* reservation offer section  */}
-              {reservationDetails?.offered_by_me ? (
-                <div className="flex items-start gap-4 flex-col px-8 pb-8 w-full">
-                  <div className="font-semibold">
-                    {reservationDetails?.pay_rate}$ /{" "}
-                    {reservationDetails?.pay_duration} offer to :
-                  </div>
-                  <div className="flex items-center pt-3 gap-4">
-                    <div className=" !rounded-full overflow-hidden w-14 h-14">
-                      {reservationDetails?.offered_to?.photo_url != null ? (
-                        <img src={reservationDetails?.offered_to?.photo_url} />
-                      ) : (
-                        <div className="w-14 h-14 flex justify-center capitalize items-center bg-slate-700 text-white">
-                          <p className="">
-                            {reservationDetails?.offered_to?.about_me}
-                          </p>
-                          <p className="">
-                            {reservationDetails?.offered_to?.about_me}
-                          </p>
-                          {reservationDetails.offered_to?.firstname.charAt(0)}
-                        </div>
-                      )}
-                    </div>
-                    <div className="">
-                      <p className="font-bold text-xl capitalize">
-                        {reservationDetails.offered_to?.firstname}{" "}
-                        {reservationDetails.offered_to?.lastname}
-                      </p>
-                    </div>
-                  </div>
-                </div>
-              ) : (
-                <div className="flex items-start gap-4 flex-col px-8 pb-8 w-full">
-                  <div className="font-semibold">
-                    {reservationDetails?.pay_rate}$ /{" "}
-                    {reservationDetails?.pay_duration} offer by :
-                  </div>
-                  <div className="flex items-center pt-3 gap-4">
-                    <div className=" !rounded-full overflow-hidden w-14 h-14">
-                      {reservationDetails?.offered_by?.photo_url != null ? (
-                        <img src={reservationDetails?.offered_by?.photo_url} />
-                      ) : (
-                        <div className="w-14 h-14 flex justify-center capitalize items-center bg-slate-700 text-white">
-                          <p className="">
-                            {reservationDetails?.offered_by?.about_me}
-                          </p>
-                          <p className="">
-                            {reservationDetails?.offered_by?.about_me}
-                          </p>
-                          {reservationDetails.offered_by?.firstname.charAt(0)}
-                        </div>
-                      )}
-                    </div>
-                    <div className="">
-                      <p className="font-bold text-xl capitalize">
-                        {reservationDetails.offered_by?.firstname}{" "}
-                        {reservationDetails.offered_by?.lastname}
-                      </p>
-                    </div>
-                  </div>
-                </div>
-              )}
-            </div>
-            <div className="border"></div>
-            {reservationDetails?.counterOffers?.map((item, index) => (
-              <div className="flex justify-start w-full flex-col p-8 ">
-                <div className="flex flex-col items-start gap-4 pb-4 ">
+        {!loading ? (
+          <div className="grid grid-cols-1 justify-between w-full py-10 gap-9 flex-wrap">
+            <div className="border-neutral-900 border h-fit w-full flex border-l  hover:shadow-sm  rounded-xl">
+              {" "}
+              <div className="flex justify-start w-full flex-col ">
+                <div className="flex flex-col items-start gap-4 p-8">
                   <div className="">
-                    <p className="font-bold text-[#2676BC] text-xl">
+                    <div className="font-bold text-[#2676BC] relative w-fit text-xl flex items-center gap-1">
                       {" "}
-                      Offer History,
-                    </p>
+                      Offer:
+                      <div className="  absolute -right-28 px-6 bg-blue-50 !text-blue-600 border-blue-600 capitalize border rounded-full py-[2px] text-sm">
+                        {reservationDetails.status}
+                      </div>
+                    </div>
+                    <div className="flex flex-row gap-3 pt-4">
+                      <div className="font-semibold">Location:</div>
+                      <p className="">{reservationDetails.location}</p>
+                    </div>
+                    <div className="flex flex-row gap-3 pt-4">
+                      <div className="font-semibold">Date:</div>
+                      <div className="flex">
+                        <p className="">
+                          {dayjs(reservationDetails.start_date).format(
+                            "DD/MM/YYYY"
+                          )}
+                        </p>
+                        <span className="px-2">-</span>
+                        <p className="">
+                          {dayjs(reservationDetails.end_date).format(
+                            "DD/MM/YYYY"
+                          )}
+                        </p>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="flex gap-3 w-full justify-start">
+                    <Link to={`/chats?${reservationDetails?.uuid}`}>
+                      <button className="px-6 py-3 bg-blue-600 text-white rounded-lg mt-2">
+                        Chat
+                      </button>
+                    </Link>
+
+                    {opentoAcceptoffer?.offered_by_me === false &&
+                      opentoAcceptoffer?.status === "open" && (
+                        <>
+                          <div
+                            onClick={() =>
+                              setTerms({ ...terms, toggle: !terms.toggle })
+                            }
+                          >
+                            <button className="px-6 py-3 bg-blue-600 text-white rounded-lg mt-2">
+                              Accept
+                            </button>
+                          </div>
+
+                          <Link
+                            to={`/counter-offer?${reservationDetails?.uuid}`}
+                          >
+                            <button className="px-6 py-3 bg-blue-600 text-white rounded-lg mt-2">
+                              Counter
+                            </button>
+                          </Link>
+                        </>
+                      )}
+                  </div>
+                  <div className="flex flex-row gap-3 pt-4">
+                    <div className="font-semibold">Description:</div>
+                    <p className="">{reservationDetails?.description}</p>
                   </div>
                 </div>
-                <div className="flex flex-col gap-4">
-                  <div className="flex items-start gap-4 flex-col p-7 border rounded-lg w-full">
-                    <div className="  -right-24 -mt-3 px-6 bg-blue-50 !text-blue-600 border-blue-600 capitalize border rounded-full py-[2px] text-sm">
-                      {item.status}
+
+                {/* reservation offer section  */}
+                {reservationDetails?.offered_by_me ? (
+                  <div className="flex items-start gap-4 flex-col px-8 pb-8 w-full">
+                    <div className="font-semibold">
+                      {reservationDetails?.pay_rate}$ /{" "}
+                      {reservationDetails?.pay_duration} offer to :
                     </div>
-                    <div className="flex flex-row justify-between  gap-3">
-                      <div className="font-semibold text-[#2676BC]">
-                        Counter Offer: {item?.pay_rate}$ / {item?.pay_duration}
+                    <div className="flex items-center pt-3 gap-4">
+                      <div className=" !rounded-full overflow-hidden w-14 h-14">
+                        {reservationDetails?.offered_to?.photo_url != null ? (
+                          <img
+                            src={reservationDetails?.offered_to?.photo_url}
+                          />
+                        ) : (
+                          <div className="w-14 h-14 flex justify-center capitalize items-center bg-slate-700 text-white">
+                            <p className="">
+                              {reservationDetails?.offered_to?.about_me}
+                            </p>
+                            <p className="">
+                              {reservationDetails?.offered_to?.about_me}
+                            </p>
+                            {reservationDetails.offered_to?.firstname.charAt(0)}
+                          </div>
+                        )}
                       </div>
-                      <div className="font-semibold items-center flex">
-                        Date:
-                        <div className="flex px-2">
-                          {dayjs(item.start_date).format("DD/MM/YYYY")}
+                      <div className="">
+                        <p className="font-bold text-xl capitalize">
+                          {reservationDetails.offered_to?.firstname}{" "}
+                          {reservationDetails.offered_to?.lastname}
+                        </p>
+                      </div>
+                    </div>
+                  </div>
+                ) : (
+                  <div className="flex items-start gap-4 flex-col px-8 pb-8 w-full">
+                    <div className="font-semibold">
+                      {reservationDetails?.pay_rate}$ /{" "}
+                      {reservationDetails?.pay_duration} offer by :
+                    </div>
+                    <div className="flex items-center pt-3 gap-4">
+                      <div className=" !rounded-full overflow-hidden w-14 h-14">
+                        {reservationDetails?.offered_by?.photo_url != null ? (
+                          <img
+                            src={reservationDetails?.offered_by?.photo_url}
+                          />
+                        ) : (
+                          <div className="w-14 h-14 flex justify-center capitalize items-center bg-slate-700 text-white">
+                            <p className="">
+                              {reservationDetails?.offered_by?.about_me}
+                            </p>
+                            <p className="">
+                              {reservationDetails?.offered_by?.about_me}
+                            </p>
+                            {reservationDetails.offered_by?.firstname.charAt(0)}
+                          </div>
+                        )}
+                      </div>
+                      <div className="">
+                        <p className="font-bold text-xl capitalize">
+                          {reservationDetails.offered_by?.firstname}{" "}
+                          {reservationDetails.offered_by?.lastname}
+                        </p>
+                      </div>
+                    </div>
+                  </div>
+                )}
+              </div>
+              <div className="border"></div>
+              {reservationDetails?.counterOffers?.map((item, index) => (
+                <div className="flex justify-start w-full flex-col p-8 ">
+                  <div className="flex flex-col items-start gap-4 pb-4 ">
+                    <div className="">
+                      <p className="font-bold text-[#2676BC] text-xl">
+                        {" "}
+                        Offer History,
+                      </p>
+                    </div>
+                  </div>
+                  <div className="flex flex-col gap-4">
+                    <div className="flex items-start gap-4 flex-col p-7 border rounded-lg w-full">
+                      <div className="  -right-24 -mt-3 px-6 bg-blue-50 !text-blue-600 border-blue-600 capitalize border rounded-full py-[2px] text-sm">
+                        {item.status}
+                      </div>
+                      <div className="flex flex-row justify-between  gap-3">
+                        <div className="font-semibold text-[#2676BC]">
+                          Counter Offer: {item?.pay_rate}$ /{" "}
+                          {item?.pay_duration}
+                        </div>
+                        <div className="font-semibold items-center flex">
+                          Date:
+                          <div className="flex px-2">
+                            {dayjs(item.start_date).format("DD/MM/YYYY")}
+                          </div>
+                        </div>
+                      </div>
+                      <div className="flex flex-col items-start pt-3 gap-4">
+                        <div className="font-bold text-xl capitalize">
+                          Made By: {item?.offered_to?.firstname}{" "}
+                          {item?.offered_to?.lastname}
+                        </div>
+                        <div className="flex justify-start">
+                          Notes:
+                          <p className="">{item?.offered_to?.about_me}</p>
                         </div>
                       </div>
                     </div>
-                    <div className="flex flex-col items-start pt-3 gap-4">
-                      <div className="font-bold text-xl capitalize">
-                        Made By: {item?.offered_to?.firstname}{" "}
-                        {item?.offered_to?.lastname}
-                      </div>
-                      <div className="flex justify-start">
-                        Notes:
-                        <p className="">{item?.offered_to?.about_me}</p>
-                      </div>
-                    </div>
                   </div>
                 </div>
-              </div>
-            ))}
+              ))}
+            </div>
           </div>
-        </div>
+        ) : (
+          <div className="flex w-full justify-center center flex-col items-center gap-5 border p-12 bg-slate-50 rounded-md mt-8">
+            <img src={emptyState} className="w-32" />
+            <div className="text-3xl ">Your reservations will appear here</div>
+          </div>
+        )}
       </div>
 
       {terms.toggle && (
