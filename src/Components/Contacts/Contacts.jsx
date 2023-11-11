2359faac6cc79da2df9ae2e579aed640fcf66f16
zahoor248 <zahoor.alphasquad@gmail.com>
2023-11-11 11:19:04 +0500

    contract details fixes

--- src/Components/Contacts/Contacts.jsx
+++ src/Components/Contacts/Contacts.jsx
@@ -18,7 +18,7 @@ const Contacts = () => {
     handleAPIRequest("get", "contract", null)
       .then((response) => {
         if (response.data) {
-          dispatch(setContracts([])); // Update Redux store with empty array
+          dispatch(setContracts(response.data.contracts)); // Update Redux store with empty array
         } else {
           console.log(response, "Here is the response");
           dispatch(setContracts(response));
@@ -34,162 +34,113 @@ const Contacts = () => {
     <div className="flex main-container h-[calc(100vh-147px)] md:h-[calc(100vh-148px)]  xl:h-[calc(100vh-160px)] 2xl:h-[calc(100vh-202px)] overflow-auto w-full">
       <div className="flex w-full flex-col    py-14">
         <div className=" justify-center items-start text-neutral-700 flex w-full">
-          <div className="text-3xl">Active Contacts</div>
+          <div className="text-3xl">Active Conrtracts</div>
         </div>
 
         <div className="grid grid-cols-1 justify-between w-full py-10 gap-9 flex-wrap">
-          <div className="border-neutral-900 border h-fit w-full flex border-l  hover:shadow-sm  rounded-xl">
-            {" "}
-            <div className="flex justify-start w-full flex-col ">
-              <div className="flex flex-col items-start gap-4 p-8">
-                <div className="">
-                  <div className="font-bold text-[#2676BC] relative w-fit text-xl flex items-center gap-1">
-                    {" "}
-                    Offer:
-                    <div className=" absolute -right-24 px-6 bg-blue-50 !text-blue-600 border-blue-600 capitalize border rounded-full py-[2px] text-sm">
-                      {contracts?.status}
-                    </div>
-                  </div>
-                  <div className="flex flex-row gap-3 pt-4">
-                    <div className="font-semibold">Location:</div>
-                    <p className="">{contracts?.location}</p>
-                  </div>
-                  <div className="flex flex-row gap-3 pt-4">
-                    <div className="font-semibold">Date:</div>
-                    <div className="flex">
-                      <p className="">
-                        {dayjs(contracts?.start_date).format(
-                          "DD/MM/YYYY"
-                        )}
-                      </p>
-                      <span className="px-2">-</span>
-                      <p className="">
-                        {dayjs(contracts?.end_date).format(
-                          "DD/MM/YYYY"
-                        )}
-                      </p>
-                    </div>
-                  </div>
-                </div>
-                <div className="flex gap-3 w-full justify-start">
-                  <Link to={`/chats?${contracts?.uuid}`}>
-                    <button className="px-6 py-3 bg-blue-600 text-white rounded-lg mt-2">
-                      Chat
-                    </button>
-                  </Link>
-               
-                </div>
-                <div className="flex flex-row gap-3 pt-4">
-                  <div className="font-semibold">Description:</div>
-                  <p className="">{contracts?.description}</p>
-                </div>
-              </div>
-
-              {/* reservation offer section  */}
-              {contracts?.offered_by_me ? (
-                <div className="flex items-start gap-4 flex-col px-8 pb-8 w-full">
-                  <div className="font-semibold">
-                    {contracts?.pay_rate}$ /{" "}
-                    {contracts?.pay_duration} offer to :
-                  </div>
-                  <div className="flex items-center pt-3 gap-4">
-                    <div className=" !rounded-full overflow-hidden w-14 h-14">
-                      {contracts?.offered_to?.photo_url != null ? (
-                        <img src={contracts?.offered_to?.photo_url} />
-                      ) : (
-                        <div className="w-14 h-14 flex justify-center capitalize items-center bg-slate-700 text-white">
-                          <p className="">
-                            {contracts?.offered_to?.about_me}
-                          </p>
-                          <p className="">
-                            {contracts?.offered_to?.about_me}
-                          </p>
-                          {contracts?.offered_to?.firstname.charAt(0)}
-                        </div>
-                      )}
-                    </div>
-                    <div className="">
-                      <p className="font-bold text-xl capitalize">
-                        {contracts?.offered_to?.firstname}{" "}
-                        {contracts?.offered_to?.lastname}
-                      </p>
-                    </div>
-                  </div>
-                </div>
-              ) : (
-                <div className="flex items-start gap-4 flex-col px-8 pb-8 w-full">
-                  <div className="font-semibold">
-                    {contracts?.pay_rate}$ /{" "}
-                    {contracts?.pay_duration} offer by :
-                  </div>
-                  <div className="flex items-center pt-3 gap-4">
-                    <div className=" !rounded-full overflow-hidden w-14 h-14">
-                      {contracts?.offered_by?.photo_url != null ? (
-                        <img src={contracts?.offered_by?.photo_url} />
-                      ) : (
-                        <div className="w-14 h-14 flex justify-center capitalize items-center bg-slate-700 text-white">
-                          <p className="">
-                            {contracts?.offered_by?.about_me}
-                          </p>
-                          <p className="">
-                            {contracts?.offered_by?.about_me}
-                          </p>
-                          {contracts?.offered_by?.firstname.charAt(0)}
-                        </div>
-                      )}
-                    </div>
-                    <div className="">
-                      <p className="font-bold text-xl capitalize">
-                        {contracts?.offered_by?.firstname}{" "}
-                        {contracts?.offered_by?.lastname}
-                      </p>
-                    </div>
-                  </div>
-                </div>
-              )}
-            </div>
-            <div className="border"></div>
-            {contracts?.counterOffers?.map((item, index) => (
-              <div className="flex justify-start w-full flex-col p-8 ">
-                <div className="flex flex-col items-start gap-4 pb-4 ">
+          {contracts?.map((item, index) => (
+            <div className="border-neutral-900 border h-fit w-full  hover:shadow-sm  rounded-xl">
+              {" "}
+              <div className="flex justify-startw-full ">
+                <div className="flex items-start gap-4 border-r w-full p-8">
                   <div className="">
-                    <p className="font-bold text-[#2676BC] text-xl">
-                      {" "}
-                      Offer History,
-                    </p>
-                  </div>
-                </div>
-                <div className="flex flex-col gap-4">
-                  <div className="flex items-start gap-4 flex-col p-7 border rounded-lg w-full">
-                    <div className="  -right-24 -mt-3 px-6 bg-blue-50 !text-blue-600 border-blue-600 capitalize border rounded-full py-[2px] text-sm">
-                      {item.status}
-                    </div>
-                    <div className="flex flex-row justify-between  gap-3">
-                      <div className="font-semibold text-[#2676BC]">
-                        Counter Offer: {item?.pay_rate}$ / {item?.pay_duration}
+                    <div className="flex items-center gap-3">
+                      <p className="font-bold text-[#2676BC] text-xl">
+                        {" "}
+                        Contract #: {item.id}
+                      </p>
+                      <div className="  px-6 bg-blue-50 !text-blue-600 border-blue-600 capitalize border rounded-full py-[2px] text-sm">
+                        {item.business.status}
                       </div>
-                      <div className="font-semibold items-center flex">
-                        Date:
-                        <div className="flex px-2">
-                          {dayjs(item.start_date).format("DD/MM/YYYY")}
+                    </div>
+                    <div className="  flex flex-col ">
+                      <div className="py-2 pt-8 text-xl font-bold">
+                        Contract Details:
+                      </div>
+                      <div className="flex text-neutral-700 flex-row gap-3 pt-2">
+                        <div className="font-semibold">Location:</div>
+                        <p className="">{item.location}</p>
+                      </div>
+                      <div className="flex text-neutral-700 flex-row gap-3 pt-4">
+                        <div className="font-semibold">Date:</div>
+                        <div className="flex">
+                          <p className="">
+                            {dayjs(item.start_date).format("DD/MM/YYYY")}
+                          </p>
+                          <span className="px-2">-</span>
+                          <p className="">
+                            {dayjs(item.end_date).format("DD/MM/YYYY")}
+                          </p>
                         </div>
                       </div>
-                    </div>
-                    <div className="flex flex-col items-start pt-3 gap-4">
-                      <div className="font-bold text-xl capitalize">
-                        Made By: {item?.offered_to?.firstname}{" "}
-                        {item?.offered_to?.lastname}
-                      </div>
-                      <div className="flex justify-start">
-                        Notes:
-                        <p className="">{item?.offered_to?.about_me}</p>
+                      <div className="pt-4 text-neutral-700 flex gap-2">
+                        {" "}
+                        <div className="font-semibold">Description:</div>
+                        {item.description}
                       </div>
                     </div>
+
+                    <div className="pt-4 flex flex-col gap-2">
+                      {" "}
+                      <div className="text-xl font-bold">Contract Details:</div>
+                      {item.additional_terms}
+                    </div>
+                  </div>
+                </div>
+                <div className="flex items-start gap-4 w-full flex-col p-8 ">
+                  <div className="font-bold text-[#2676BC] text-xl">
+                    {/* <GoPrimitiveDot className='online-icon'/> */}
+                    Contract Between:
+                  </div>
+                  <div className="flex items-center pt-3 gap-4">
+                    <div className=" !rounded-full overflow-hidden w-14 h-14">
+                      {item.business?.photo_url != null ? (
+                        <img src={item.business?.photo_url} />
+                      ) : (
+                        <div className="w-14 h-14 flex justify-center capitalize items-center bg-slate-700 text-white">
+                          {item?.business?.firstname.charAt(0)}
+                        </div>
+                      )}
+                      {/* <GoPrimitiveDot className='online-icon'/> */}
+                    </div>
+                    <div className="">
+                      <p className="font-bold text-xl capitalize">
+                        {item?.business?.firstname} {item?.business?.lastname}
+                      </p>
+                      <p className="">{item?.business?.about_me}</p>
+                    </div>
+                  </div>
+                  <div className="flex items-center pt-3 gap-4">
+                    <div className=" !rounded-full overflow-hidden w-14 h-14">
+                      {item.pro?.photo_url != null ? (
+                        <img src={item.pro?.photo_url} />
+                      ) : (
+                        <div className="w-14 h-14 flex justify-center capitalize items-center bg-slate-700 text-white">
+                          {item?.pro?.firstname.charAt(0)}
+                        </div>
+                      )}
+                      {/* <GoPrimitiveDot className='online-icon'/> */}
+                    </div>
+                    <div className="">
+                      <p className="font-bold text-xl capitalize">
+                        {item?.pro?.firstname} {item?.pro?.lastname}
+                      </p>
+                      <p className="">
+                        {item?.pro?.about_me ? item?.pro?.about_me : "N/A"}
+                      </p>
+                    </div>
+                  </div>
+                  <div className="flex w-full justify-end">
+                    <Link to={`/reservation-detail?${item?.uuid}`}>
+                      <button className="px-6 py-3 bg-blue-600 text-white rounded-lg mt-2">
+                        Cancel Contract
+                      </button>
+                    </Link>
                   </div>
                 </div>
               </div>
-            ))}
-          </div>
+            </div>
+          ))}
         </div>
       </div>
     </div>
