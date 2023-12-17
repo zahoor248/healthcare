4f8c2b29700352aa5a9abb6b23dd071cdd4fea99
shassan123 <hassanaltaf.alphasquad@gmail.com>
2023-12-17 20:44:12 +0500

    changes

--- src/Components/Reservations/ReservationDetails.jsx
+++ src/Components/Reservations/ReservationDetails.jsx
@@ -510,7 +510,8 @@ const ReservationDetails = () => {
                                 <path d="M20 10c0 6-8 12-8 12s-8-6-8-12a8 8 0 0 1 16 0Z" />
                                 <circle cx="12" cy="10" r="2" fill="#ffffff" />
                               </svg>
-
+                              
+ 
                               <span className="font-normal text-sm md:text-base text-neutral-500">
                                 Location:
                               </span>
